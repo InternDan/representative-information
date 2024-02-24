c3c62d9c56afa05f811d117c3fef16066e9fc204
InternDan <daniel.leib@gmail.com>
2024-02-24 14:24:32 -0600

    started

---
+++ src/main/java/com/danielleib/representativeinfo/repinfo/RepInfoApplication.java
@@ -0,0 +1,13 @@
+package com.danielleib.representativeinfo.repinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielleib/representativeinfo/repinfo/RepInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielleib.representativeinfo.repinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
